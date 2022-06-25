b9d070666ddd6f7968d0c4169f4f176cbbfeab51
Irene Riportella <riportellairene@gmail.com>
2022-06-25 21:27:03 +0200

    created decrypt function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-let encryptBtn = document.getElementById('encryptBtn');
 let encrypted;
+let decrypted;
+
+let encryptBtn = document.getElementById('encryptBtn');
 encryptBtn.addEventListener('click', () => {
+  cleanOutputArea();
   encrypt();
-  //   showOutput();
 });
 
-// let decryptBtn = document.getElementById('decryptBtn');
-// decryptBtn.addEventListener('click', () => {
-//   decrypt();
-// });
+let decryptBtn = document.getElementById('decryptBtn');
+decryptBtn.addEventListener('click', () => {
+  cleanOutputArea();
+  decrypt();
+});
 
 // Function to encrypt the text
 function encrypt(inputValue) {
@@ -17,35 +20,62 @@ function encrypt(inputValue) {
 
   // Transform the text to lowercase
   let textToEncrypt = inputValue.toLowerCase();
-  console.log(textToEncrypt);
 
   // Encrypt the text
   encrypted = textToEncrypt.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
 
   //   let encrypted = textToEncrypt.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
 
-  console.log(encrypted);
-
+  // Clean input space
   document.querySelector('.input-space').value = '';
 
+  // Call the showOutput function
   showOutput();
 
   return encrypted;
 }
 
+// Function to show the result of encryption
 function showOutput() {
+  // Clean the the image and its caption
   let figureArea = document.getElementById('figure');
   figureArea.style.display = 'none';
 
+  // Create and style a text area where we will show the encrypted text
   let resultArea = document.getElementById('result-box');
   resultArea.insertAdjacentHTML('afterbegin', '<textarea autofocus class="result-box"></textarea>');
 
   resultArea.style.padding = '2% 0 2% 0';
 
+  // Show the encrypted text in the text area
   let showResult = document.querySelector('.result-box');
+
   showResult.value = encrypted;
 }
 
-// function decrypt() {
+// Function to clean the output area in case there is already some encrypted text
+function cleanOutputArea() {
+  if (encrypted) {
+    let outputArea = document.querySelector('.result-box');
+    outputArea.style.display = 'none';
+  }
+}
 
-// }
+function decrypt(inputValue) {
+  // Get the text entered by the user
+  inputValue = document.querySelector('.input-space').value;
+
+  // Transform the text to lowercase
+  let textToDencrypt = inputValue.toLowerCase();
+
+  // Decrypt the text
+  decrypted = textToDencrypt.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+
+  // Clean input space
+  document.querySelector('.input-space').value = '';
+
+  // Call the showOutput function
+  showOutput();
+
+  return decrypted;
+}
