8747947269ee9f41acb630910549fc39f6558e85
Irene Riportella <riportellairene@gmail.com>
2022-06-26 10:07:06 +0200

    created copyResult function

--- js/app.js
+++ js/app.js
@@ -18,21 +18,26 @@ function encrypt(inputValue) {
   // Get the text entered by the user
   inputValue = document.querySelector('.input-space').value;
 
-  // Transform the text to lowercase
-  let textToEncrypt = inputValue.toLowerCase();
+  if (inputValue.length === 0) {
+    alert('Debe ingresar un texto');
+    return;
+  } else {
+    // Transform the text to lowercase
+    let textToEncrypt = inputValue.toLowerCase();
 
-  // Encrypt the text
-  encrypted = textToEncrypt.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    // Encrypt the text
+    encrypted = textToEncrypt.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
 
-  //   let encrypted = textToEncrypt.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+    //   let encrypted = textToEncrypt.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
 
-  // Clean input space
-  document.querySelector('.input-space').value = '';
+    // Clean input space
+    document.querySelector('.input-space').value = '';
 
-  // Call the showOutput function
-  showOutput();
+    // Call the showOutput function
+    showOutput();
 
-  return encrypted;
+    return encrypted;
+  }
 }
 
 // Function to show the result of encryption
@@ -52,11 +57,11 @@ function showOutput() {
 
   // Verify if user is targeting encrypt or decrypt button and send the right answer
   document.addEventListener('click', (e) => {
-    let btnTarget = e.target;
+    let targetBtn = e.target;
 
-    if (btnTarget.classList.contains('encrypt')) {
+    if (targetBtn.classList.contains('encrypt')) {
       showResult.value = encrypted;
-    } else if (btnTarget.classList.contains('decrypt')) {
+    } else if (targetBtn.classList.contains('decrypt')) {
       showResult.value = decrypted;
     }
   });
@@ -70,21 +75,37 @@ function cleanOutputArea() {
   }
 }
 
+// Function to decrypt an encrypted text
 function decrypt(inputValue) {
   // Get the text entered by the user
   inputValue = document.querySelector('.input-space').value;
 
-  // Transform the text to lowercase
-  let textToDencrypt = inputValue.toLowerCase();
+  if (inputValue.length === 0) {
+    alert('Debe ingresar un texto');
+    return;
+  } else {
+    // Transform the text to lowercase
+    let textToDencrypt = inputValue.toLowerCase();
 
-  // Decrypt the text
-  decrypted = textToDencrypt.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    // Decrypt the text
+    decrypted = textToDencrypt.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
 
-  // Clean input space
-  document.querySelector('.input-space').value = '';
+    // Clean input space
+    document.querySelector('.input-space').value = '';
 
-  // Call the showOutput function
-  showOutput();
+    // Call the showOutput function
+    showOutput();
 
-  return decrypted;
+    return decrypted;
+  }
+}
+
+// Function to copy the result of encryption/decryption
+copyResult();
+function copyResult() {
+  let targetBtn = document.querySelector('.copy');
+  targetBtn.addEventListener('click', () => {
+    let textToCopy = document.querySelector('.result-box').value;
+    navigator.clipboard.writeText(textToCopy);
+  });
 }
